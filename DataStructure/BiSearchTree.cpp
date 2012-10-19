fac6a6bdb0794905a7a99060832cb23b900a9623
Ming Yang <wentfar@126.com>
2012-10-19 20:40:52 +0800

    20121019night_lab

--- DataStructure/BiSearchTree.cpp
+++ DataStructure/BiSearchTree.cpp
@@ -9,11 +9,37 @@ struct BiNode{
 	BiNode(T element, BiNode<T> *lt, BiNode<T> *rt, BiNode<T> *pt):k_data(element), LChild(lt), RChild(rt), Parent(pt){}
 };
 
+
+//打印节点
+template <typename T>
+void PrintNode(BiNode<T> node)
+{
+	cout<<node.k_data<<" ";
+}
+
 template <typename T>
 class BiSearchTree
 {
 public:
-	BiSearchTree(BiNode<T>* pRoot = NULL){root = pRoot;}
+	BiSearchTree(BiNode<T>* pRoot = NULL)
+	{
+		root = pRoot;
+	}
+
+	~BiSearchTree()
+	{
+		ReleaseBiTree(&root);
+	}
+
+	void ReleaseBiTree(BiNode<T> **pRoot)
+	{
+		if (*pRoot != NULL)
+		{
+			ReleaseBiTree(&(*pRoot)->LChild);
+			ReleaseBiTree(&(*pRoot)->RChild);
+			delete (*pRoot);
+		}
+	}
 
 	//前序遍历二叉树
 	void VisitBiTreePreOrder(void(*Visit)(BiNode<T>)) const
@@ -80,6 +106,15 @@ public:
 		}
 	}
 
+	//创建二叉查找树
+	void CreatBiSearchTree(T a[], int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			Insert(a[i]);
+		}
+	}
+
 	//求一个节点的后继节点
 	BiNode<T>* bst_successor(BiNode<T> *node)
 	{
@@ -101,20 +136,79 @@ public:
 		}
 	}
 
-	//删除，找到此节点，删除并返回true，否则返回false
+	//删除节点
+	//1. 结点Z没有孩子结点，那么直接删除Z就行了。
+	//2. 结点Z有一个孩子结点，那么删除Z，将Z的父结点与此孩子结点（子树）关联就可以了。
+	//3. 结点Z有两个孩子结点，实现方法就是将后继从二叉树中删除（适用第一、二种情况），将后继的数据覆盖到Z中。
 	bool Delete(T key)
 	{
-		return Delete(root, key);
-	}
-	
-	//创建二叉查找树
-	void CreatBiSearchTree(T a[], int n)
-	{
-		for (int i = 0; i < n; i++)
+		if (root == NULL)
 		{
-			Insert(a[i]);
+			return false;
 		}
+		BiNode<T>* p = root;
+		while(p != NULL)//查找key
+		{
+			if (p->k_data > key)
+			{
+				p = p->LChild;
+			} 
+			else if(p->k_data < key)
+			{
+				p = p->RChild;
+			}
+			else
+			{
+				break;
+			}
+		}
+
+		if (p != NULL)//查找到k
+		{
+			//求真正被删除的节点
+			BiNode<T>* p_real;  
+			if (p->LChild && p->RChild)  
+				p_real = bst_successor(p);//此真正被删除的节点一定满足条件1.或者条件2.
+			else  
+				p_real = p;
+
+			BiNode<T>* p_parent = p_real->Parent;
+			if (p_real->LChild == NULL && p_real->RChild == NULL)//1.左右都空
+			{
+				if (p_parent == NULL)//需要删除的是根节点
+					root = NULL;
+				else 
+					if (p_parent->LChild == p_real) {p_parent->LChild = NULL;}
+					else {p_parent->RChild = NULL;}
+			} 
+			else if (p_real->LChild == NULL  && p_real->RChild != NULL)//2.左空，右不空
+			{
+				if (p_parent == NULL)//需要删除的是根节点
+					{root = p_real->RChild; p_real->RChild->Parent = NULL;}
+				else 
+					if (p_parent->LChild == p_real) {p_parent->LChild = p_real->RChild; p_real->RChild->Parent = p_parent;}
+					else {p_parent->RChild = p_real->RChild; p_real->RChild->Parent = p_parent;}
+			}
+			else if (p_real->LChild != NULL  && p_real->RChild == NULL)//3.左不空，右空
+			{
+				if (p_parent == NULL)//需要删除的是根节点
+					{root = p_real->LChild; p_real->LChild->Parent = NULL;}
+				else 
+					if (p_parent->LChild == p_real) {p_parent->LChild = p_real->LChild; p_real->LChild->Parent = p_parent;}
+					else {p_parent->RChild = p_real->LChild; p_real->LChild->Parent = p_parent;}
+			}
+
+			if (p_real != p)//如果是有双子树的节点，则覆盖原应被删除节点
+			{
+				p->k_data = p_real->k_data;
+			}
+
+			delete p_real;//删除节点
+			return true;
+		}
+		return false;
 	}
+
 
 protected:
 private:
@@ -159,89 +253,13 @@ private:
 		} 
 	}
 
-	//删除节点
-	//1. 结点Z没有孩子结点，那么直接删除Z就行了。
-	//2. 结点Z有一个孩子结点，那么删除Z，将Z的父结点与此孩子结点（子树）关联就可以了。
-	//3. 结点Z有两个孩子结点，实现方法就是将后继从二叉树中删除（适用第一、二种情况），将后继的数据覆盖到Z中。
-	bool Delete(T key)
-	{
-		if (root == NULL)
-		{
-			return false;
-		}
-		BiNode<T>* p = root;
-		while(p != NULL)//查找key
-		{
-			if (p->k_data > key)
-			{
-				p = p->LChild;
-			} 
-			else if(p->k_data < key)
-			{
-				p = p->RChild;
-			}
-			else
-			{
-				break;
-			}
-		}
-
-		if (p != NULL)//查找到k
-		{
-			//求真正被删除的节点
-			BiNode<T>* p_real;  
-			if (p->left && p->right)  
-				p_real = bst_successor(p);//此真正被删除的节点一定满足条件1.或者条件2.
-			else  
-				p_real = p;
-
-			BiNode* p_parent = p_real->Parent;
-			if (p_real->LChild == NULL && p_real->RChild == NULL)
-			{
-				if (p_parent == NULL)//只有根节点
-					root = NULL;
-				else 
-				{
-					p_parent->LChild == p_real ? p_parent->LChild = NULL : p_parent->RChild = NULL;
-				}
-			} 
-			else if (p_real->LChild == NULL  && p_real->RChild != NULL)
-			{
-				if (p_parent == NULL)
-					root = p_real->RChild;
-				else 
-					if (p_parent->LChild == p_real) {p_parent->LChild = p_real->RChild; p_real->RChild->Parent = p_parent;}
-					else {p_parent->RChild = p_real->RChild; p_real->RChild->Parent = p_parent;}
-			}
-			else if (p_real->LChild != NULL  && p_real->RChild == NULL)
-			{
-				if (p_parent == NULL)
-					root = p_real->RChild;
-				else 
-					if (p_parent->LChild == p_real) {p_parent->LChild = p_real->LChild; p_real->LChild->Parent = p_parent;}
-					else {p_parent->RChild = p_real->LChild; p_real->LChild->Parent = p_parent;}
-			}
-			delete p_real;//重要
-
-			return true;
-		}
-		return false;
-	}
-
 	BiNode<T> *root;
 };
-
-//打印节点
-template <typename T>
-void PrintNode(BiNode<T> node)
-{
-	cout<<node.k_data<<" ";
-}
 
 
 int main()
 {
-	int keys[] = {1, 4, 7, 2, 5, 8, 3, 6, 9};
+	int keys[] = {8, 5, 2, 1, 4, 7, 3, 6, 9};
 	BiSearchTree<int>* p_BiSearchTree = new BiSearchTree<int>();
 	p_BiSearchTree->CreatBiSearchTree(keys, sizeof(keys)/sizeof(keys[0]));
 	cout<<"前序遍历二叉树：";
